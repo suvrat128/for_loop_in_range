3e8f52e2f7cca0d9d5a4f2ba209c41aefa4e24ec
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-11 23:38:47 +0530

    Create 01_square_first_10.py

---
+++ 01_square_first_10.py
@@ -0,0 +1,19 @@
+#wpt print squre offirst 10 positive number
+
+for i in range(10):
+    print(i**2)
+'''
+#process
+1. using for loop with range
+    iteration 1:it will fetch 0 so i is 0 print 0**2 == 0
+    iteration 2:it will fetch 1 so i is 0 print 1**2 == 1
+    iteration 3:it will fetch 2 so i is 0 print 2**2 == 4
+    iteration 4:it will fetch 3 so i is 0 print 3**2 == 9
+    iteration 5:it will fetch 4 so i is 0 print 4**2 == 16
+    iteration 6:it will fetch 5 so i is 0 print 5**2 == 25
+    iteration 7:it will fetch 6 so i is 0 print 6**2 == 36
+    iteration 8:it will fetch 7 so i is 0 print 7**2 == 49
+    iteration 9:it will fetch 8 so i is 0 print 8**2 == 64
+    iteration 10:it will fetch 9 so i is 0 print 9**2 == 81
+  
+'''
